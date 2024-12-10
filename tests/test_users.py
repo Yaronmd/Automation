83af444da6f9a6f865a7d5bc8a5df7108d2d2e8b
Yaronmd <yaronmord@gmail.com>
2024-12-10 13:00:56 +0200

    changes

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,7 +1,20 @@
 import pytest
 from api_tests.request_helper import Request
 
+users_requests = Request(endpoint="/users")
 
 def test_get_users():
-    req = Request(endpoint="/users")
-    assert req.make_get_request().status_code == 200
+    assert users_requests.make_get_request().status_code == 200
+
+
+def test_set_user():
+    post_data = {
+        "firstname": "yaron",
+        "lastname": "mordechai",
+        "email":"yaronmord@gmail.com"
+
+    }
+    response = users_requests.make_post_request(data=post_data)
+    assert response.status_code == 201
+    print(response.json())
+   
