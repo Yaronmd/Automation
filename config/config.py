8e38741f6a222343ff1e9229bcb2bd72e9896ede
Yaronmd <yaronmord@gmail.com>
2025-01-05 15:04:11 +0200

    add checks

--- config/config.py
+++ config/config.py
@@ -1,8 +1,11 @@
+from dynaconf import settings
+
+
 BASE_URL = "https://jsonplaceholder.typicode.com"
 HEADERS = {
      "Content-Type": "application/json"
 }
 
-UI_BASE_PAGE = "https://www.saucedemo.com/"
-UI_USERNAME = "standard_user"
-UI_PASSWORD = "secret_sauce"
+UI_BASE_PAGE = settings.UI_BASE_PAGE
+UI_USERNAME = settings.UI_USERNAME
+UI_PASSWORD = settings.UI_PASSWORD
